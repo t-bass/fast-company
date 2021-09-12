b743cec1c2a45a5066128c11bfd2ed342caf6ff3
goncharovviktor <t-bass@mail.ru>
2021-09-12 20:08:13 +0300

    create fast-company

---
+++ src/app/components/bookmark.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const BookMark = ({ status, ...rest }) => {
+    return (
+        <button {...rest}>
+            <i className={'bi bi-star' + (status ? '-fill' : '')}></i>
+        </button>
+    )
+}
+
+BookMark.propTypes = {
+    status: PropTypes.bool
+}
+
+export default BookMark
